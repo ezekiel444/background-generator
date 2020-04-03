9bf90e88de33056e6101f0306c68d765c19ebbd8
Ezekiel <ezekielmatomi588@gmail.com>
2020-04-04 01:23:56 +0200

    background-generator

---
+++ script.js
@@ -0,0 +1,14 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("design");
+console.log(body);
+
+function gradient() {
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value},green, yellow`;
+  css.textContent = body.style.background;
+}
+
+color1.addEventListener("input", gradient);
+
+color2.addEventListener("input", gradient);
